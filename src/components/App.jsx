310f741de6235aa7aab300cc7797eb2112b58cdc
0qsenxx <semen.antipyuk@gmail.com>
2024-11-15 21:04:28 +0200

    lalalal

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,16 @@
 // import Nature from './Nature/Nature';
+import Header from './Header/Header';
+import Hero from './Hero/Hero';
+import WeatherList from './WeatherList/WeatherList';
 import WeatherInfo from './WeatherInfo/WeatherInfo';
 import Forecast from './Forecast/Forecast';
 
 export const App = () => {
   return (
     <>
-      <WeatherInfo />
-      <Forecast />
+      <Header />
+      <Hero />
+      <WeatherList />
     </>
   );
 };
