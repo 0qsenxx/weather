706a5bb15561a7fce1284f0530aac05103adc2a5
Devohka <andr.fedak@gmail.com>
2024-11-16 12:34:35 +0200

    header-adaptive-v1

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,98 @@
 import css from './Header.module.scss';
 import { useState } from 'react';
+import logo from "./logo.png";
+import user from "./user.png";
+
+// console.log(window.style.width);
 
 const Header = () => {
   const [isOpen, setOpen] = useState(false);
+  const [isOpenBurger, setOpenBurger] = useState(false);
 
   function isOpenModal() {
     setOpen(!isOpen);
-  }
+    // if (e.key === "Escape") {
+    //   document.addEventListener("keydown", setOpen(!isOpen));
+    // }
+  };
+
+  function isOpenClickModal(e) {
+    // setOpen(!isOpen);
+    if (e.key === "Escape") {
+      setOpen(!isOpen);
+    }
+  };
+
+  document.addEventListener("keydown", isOpenClickModal);
+
+  function isOpenBurgerF() {
+    setOpenBurger(!isOpenBurger);
+  };
 
   return (
     <>
       <header className={css.header}>
-        <div className="container">
-          <div className={css.header__logo}></div>
+        <div className={css.container}>
+          <div className={css.header__logo}>
+            <img src={logo} />
+          </div>
+
+
+          <button type='button' className={css.header__burgerBtn} onClick={isOpenBurgerF}>Menu
+            {isOpenBurger ?
+              <svg xmlns="http://www.w3.org/2000/svg" width="7" height="10" viewBox="0 0 7 10" fill="none" className={css.header__burgerSvg}>
+                <path d="M1.24219 9.24268L5.48483 5.00004L1.24219 0.757395" stroke="black" stroke-linecap="round" />
+              </svg>
+
+              :
+              <svg xmlns="http://www.w3.org/2000/svg" width="10" height="6" viewBox="0 0 10 6" fill="none" className={css.header__burgerSvg}>
+                <path d="M1 1L5.24264 5.24264L9.48528 1" stroke="black" stroke-linecap="round" />
+              </svg>
+
+            }
+          </button>
+
+
+
+
+          {isOpenBurger ?
+             <div className={css.burger}>
+             <ul className={css.header__navList}>
+               <li className={css.header__navItem}>
+                 <a className={css.header__navPage}>Who we are</a>
+               </li>
+               <li className={css.header__navItem}>
+                 <a className={css.header__navPage}>Contacts</a>
+               </li>
+               <li className={css.header__navItem}>
+                 <a className={css.header__navPage}>Menu</a>
+               </li>
+             </ul>
+             <div className={css.burger__signup}>
+               <button
+                 type="button"
+                 className={css.signup__button}
+                 onClick={isOpenModal}
+               >
+                 Sign Up
+               </button>
+               <div className={css.signup__user}>
+                 <img src={user} />
+               </div>
+             </div>
+           </div>
+
+            :
+            console.log("hide")
+
+          }
+
+
+
+         
+
+
+
           <nav className={css.header__nav}>
             <ul className={css.header__navList}>
               <li className={css.header__navItem}>
@@ -26,6 +106,8 @@ const Header = () => {
               </li>
             </ul>
           </nav>
+
+
           <div className={css.header__signup}>
             <button
               type="button"
@@ -34,14 +116,16 @@ const Header = () => {
             >
               Sign Up
             </button>
-            <div className={css.signup__user}></div>
+            <div className={css.signup__user}>
+              <img src={user} />
+            </div>
           </div>
         </div>
       </header>
 
       {isOpen ? (
-        <div className={css.backdrop}>
-          <form className={css.modal}>
+        <div className={css.backdrop} >
+          <form className={css.modal} >
             <h2 className={css.modal__title}>Sign up</h2>
             <ul className={css.modal__list}>
               <li className={css.modal__item}>
