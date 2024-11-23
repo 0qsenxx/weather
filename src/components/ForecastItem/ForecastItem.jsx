7e771d9f0a9063f17ff901fc6bcac399d73d329f
0qsenxx <semen.antipyuk@gmail.com>
2024-11-23 12:30:40 +0200

    forecast styles and adaptive

--- src/components/ForecastItem/ForecastItem.jsx
+++ src/components/ForecastItem/ForecastItem.jsx
@@ -1,15 +1,17 @@
 import css from './ForecastItem.module.scss';
+import lightRainImg from '../../imgs/icons/lightRain.png';
 
 const ForecastItem = () => {
   return (
-    <li className={css.forecast__item}>
-      <p className={`${css.forecast__text} ${css.forecast__date}`}>
-        Fri, Oct 13
-      </p>
-      <div className={css['forecast__item-box']} >
-        <img className={css.forecast__icon} src={'lala'} alt={'l'} />
-        <p className={css.forecast__item}></p>
+    <li className={css.forecastItem}>
+      <p className={`${css.forecastText} ${css.forecastDate}`}>Fri, Oct 13</p>
+      <div className={css.forecastItemBox}>
+        <img className={css.forecastIcon} src={lightRainImg} alt={'l'} />
+        <p className={css.forecastText}>23/14℃</p>
       </div>
+      <p className={`${css.forecastText} ${css.forecastWeatherText}`}>light rain</p>
     </li>
   );
 };
+
+export default ForecastItem;
